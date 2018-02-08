2be25d908150ee1d611c233477064d39ec820360
KyleLEI <leizhao2@gmail.com>
2018-02-08 15:41:22 +0800

    actually a reset sets things right

--- drivers/sensors/mpu6050.c
+++ drivers/sensors/mpu6050.c
@@ -146,22 +146,20 @@ static int mpu6050_i2c_read(FAR struct mpu6050_dev_s *priv,
   config.address   = priv->addr;
   config.addrlen   = 7;
 
-  uint8_t mpu_addr;
-
   /* Write the register address to read from */
 
-  for(mpu_addr=71;mpu_addr<127;mpu_addr++){
-  	  config.address=mpu_addr;
-  	  ret = i2c_write(priv->i2c, &config, &regaddr, 1);
-  	  if(ret>=0) break;
-    }
-//  	  if (ret < 0)
-//  	    {
-//  		  syslog(LOG_ERR,"i2c_write to failed: %d\n", ret);
-//  	      return ret;
-//  	    }
+//  for(mpu_addr=71;mpu_addr<127;mpu_addr++){
+  	  config.address=priv->addr;
+  	ret = i2c_write(priv->i2c, &config, &regaddr, 1);
+//  	  if(ret>=0) break;
+//    }
+  	  if (ret < 0)
+  	    {
+  		  syslog(LOG_ERR,"i2c_read write part failed: %d\n", ret);
+  	      return ret;
+  	    }
 
-    syslog(LOG_INFO,"INFO: MPU-6050 found at: 0x%02X, reading reg 0x%02X\n", mpu_addr,regaddr);
+    syslog(LOG_INFO,"INFO: MPU-6050 found at: 0x%02X, reading reg 0x%02X\n", config.address,regaddr);
 
   /* Read "len" bytes from regaddr */
 
@@ -201,19 +199,20 @@ static int mpu6050_i2c_write(FAR struct mpu6050_dev_s *priv, uint8_t* regval,int
 
 //  /* Write 8 bits to device */
 
-  uint8_t mpu_addr;
-
-    for(mpu_addr=71;mpu_addr<127;mpu_addr++){
-  	  config.address=mpu_addr;
+//  uint8_t mpu_addr;
+//
+//    for(mpu_addr=71;mpu_addr<127;mpu_addr++){
+//  	  config.address=mpu_addr;
+    	  config.address=priv->addr;
   	  ret=i2c_write(priv->i2c, &config, regval, len);
-  	  if(ret>=0) break;
-    }
+//  	  if(ret>=0) break;
+//    }
 
   	  if (ret < 0)
   	    {
   	      syslog(LOG_ERR,"ERROR: i2c_write failed: %d\n", ret);
   	    }
-    syslog(LOG_INFO,"MPU-6050 found at: 0x%02X, writing 0x%02X %d\n", mpu_addr,*regval);
+    syslog(LOG_INFO,"MPU-6050 found at: 0x%02X, writing 0x%02X %d\n", config.address,*regval);
   return ret;
 }
 
